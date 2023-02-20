7d9d4305ee67ae17739d36ef4b18d9fc96113802
Juliette Chevalier <juliettech.1393@gmail.com>
2023-02-20 17:38:08 -0500

    get members and list them

--- src/components/MembersList/index.tsx
+++ src/components/MembersList/index.tsx
@@ -1,7 +1,25 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Table } from 'react-bootstrap';
+import { ContextPlugin, TokenVotingClient } from '@aragon/sdk-client';
+import { useAragonSDKContext } from '../../context/AragonSDK';
 
 export default function MembersList(): JSX.Element {
+  const [members, setMembers] = useState<string[]>([]);
+
+  const { context } = useAragonSDKContext();
+
+  useEffect(() => {
+    async function getDaoMembers() {
+      const contextPlugin: ContextPlugin = ContextPlugin.fromContext(context);
+      const tokenVotingClient: TokenVotingClient = new TokenVotingClient(contextPlugin);
+
+      const daoAddressorEns: string = "0x16c6e7a2082e5f4f9fb96415b748ec7e20b9da87"; // or my-dao.dao.eth
+      const daoMembers: string[] = await tokenVotingClient.methods.getMembers(daoAddressorEns);
+      setMembers(daoMembers);
+    };
+    getDaoMembers();
+  }, [context]);
+
   return (
     <>
       <h3 className="text-center pt-5">DAO Members</h3>
@@ -14,14 +32,12 @@ export default function MembersList(): JSX.Element {
           </tr>
         </thead>
         <tbody>
-          <tr>
-            <td>1</td>
-            <td>Mark</td>
-          </tr>
-          <tr>
-            <td>2</td>
-            <td>Jacob</td>
-          </tr>
+          {members.map((member, index) => (
+            <tr key={index}>
+              <td>{index + 1}</td>
+              <td>{member}</td>
+            </tr>
+          ))}
         </tbody>
       </Table>
     </>
