f6776e3564d0e2e3ae3788adf644acdd0987efaa
Juliette Chevalier <juliettech.1393@gmail.com>
2023-02-28 19:30:06 -0700

    get proposal done

--- src/components/DisplayProposals/index.tsx
+++ src/components/DisplayProposals/index.tsx
@@ -1,24 +1,69 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Button, Card, Container, Row } from 'react-bootstrap';
+import { ContextPlugin, IVoteProposalParams, TokenVotingClient, TokenVotingProposalListItem, VoteProposalStep, VoteValues } from '@aragon/sdk-client';
+
+import { useAragonSDKContext } from '../../context/AragonSDK';
 
 export default function DisplayProposals() {
+  const [proposals, setProposals] = useState<TokenVotingProposalListItem[]>([]);
+
+  const { context } = useAragonSDKContext();
+  const contextPlugin: ContextPlugin = ContextPlugin.fromContext(context);
+  const tokenVotingClient: TokenVotingClient = new TokenVotingClient(contextPlugin);
+
+  useEffect(() => {
+    async function fetchProposals() {
+      const daoProposals: TokenVotingProposalListItem[] = await tokenVotingClient.methods.getProposals({ daoAddressOrEns: "0xff25e3d89995ea3b97cede27f00ec2281a89e960" });
+      setProposals(daoProposals);
+    }
+    fetchProposals();
+  });
+
+  async function vote(proposalId: string, voteInput: VoteValues) {
+    const voteParams: IVoteProposalParams = {
+      proposalId,
+      vote: voteInput
+    };
+
+    const steps = tokenVotingClient.methods.voteProposal(voteParams);
+
+    for await (const step of steps) {
+      try {
+        switch (step.key) {
+          case VoteProposalStep.VOTING:
+            alert(`Voting... Review your transaction here: https://goerli.etherscan.io/tx/${step.txHash}`);
+            break;
+          case VoteProposalStep.DONE:
+            alert(`Vote casted for proposal ${proposalId}!`);
+            break;
+        }
+      } catch (err) {
+        console.error(err);
+      }
+    }
+  }
+
   return (
     <Container className="mx-auto">
       <h3 className="text-center pt-5 pb-3">DAO Proposals</h3>
       <Row>
-        <Card border="success" className="mx-3 mb-5" style={{ width: 'auto' }}>
-          <Card.Img variant="top" src="holder.js/100px180" />
-          <Card.Body>
-            <Card.Title>Proposal Name</Card.Title>
-            <Card.Text>
-              Proposal Description
-            </Card.Text>
-            <div className="d-flex justify-content-between">
-              <Button variant="primary">Yay</Button>
-              <Button variant="warning">Nay</Button>
-            </div>
-          </Card.Body>
-        </Card>
+        {proposals.map((proposal, index) => {
+          const { id, metadata } = proposal;
+          return (
+            <Card border="success" className="mx-3 mb-5" style={{ width: 'auto' }} key={index}>
+              <Card.Body>
+                <Card.Title>{metadata.title}</Card.Title>
+                <Card.Text>
+                  {metadata.summary}
+                </Card.Text>
+                <div className="d-flex justify-content-between">
+                  <Button variant="primary" onClick={() => vote(id, VoteValues.YES)}>Yay</Button>
+                  <Button variant="warning" onClick={() => vote(id, VoteValues.NO)}>Nay</Button>
+                </div>
+              </Card.Body>
+            </Card>
+          )
+          })}
       </Row>
     </Container>
   )
